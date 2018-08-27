3a27d2ef72d7e24378268f4bf7e25538abe72f91
Joel Smith <joelwsmith123@gmail.com>
2018-08-27 15:33:14 -0600

    Add new tests and functionality

--- test/Node-test.js
+++ test/Node-test.js
@@ -1,27 +1,27 @@
 import { expect } from 'chai';
-
 import Node from '../lib/Node';
 
-describe ('NODE', => {
+describe ('NODE', () => {
   let node;
 
-  beforeEach ( => {
+  beforeEach ( () => {
     node = new Node('p');
   })
 
-  it.skip('should exist', => {
+  it('should exist', () => {
     expect(node).to.exist;
   })
 
-  it.skip('should take a letter as an argument and assign it to the letter property', => {
+  it('should take a letter as an argument and assign it to the letter property', () => {
     expect(node.letter).to.equal('p');
   })
 
-  it.skip('should default end to false', => {
+  it('should default end to false', () => {
     expect(node.end).to.equal(false);
   })
 
-  it.skip('should default children to null', => {
+  it('should default children to null', () => {
     expect(node.children).to.equal(null);
   })
+
 })
--- test/Trie-test.js
+++ test/Trie-test.js
@@ -1,2 +1,28 @@
-import { assert } from 'chai';
+import { expect } from 'chai';
+import Trie from '../lib/trie';
 
+describe('TRIE', () => {
+  let trie;
+
+  beforeEach( () => {
+    trie = new Trie();
+  })
+
+  it('should exist', () => {
+    expect(trie).to.exist;
+  })
+
+  it('should default length to zero', () => {
+    expect(trie.length).to.equal(0);
+  })
+
+  it('should default root to null', () => {
+    expect(trie.root).to.equal(null);
+  })
+
+  it('should increase count for each new word instantiated', () => {
+    expect(trie.length).to.equal(0);
+    trie.insert('whooohooo');
+    expect(trie.length).to.equal(1);
+  })
+})
