d7ca367358784f58f6993deacbf20ef48d91aee5
Joel Smith <joelwsmith123@gmail.com>
2018-08-29 17:19:26 -0600

    Add populate method for dictionary content

--- test/Trie-test.js
+++ test/Trie-test.js
@@ -1,5 +1,16 @@
 const { expect } = require('chai');
 const Trie = require('../lib/Trie');
+const fs = require('fs');
+
+const text = "/usr/share/dict/words";
+const dictionary = fs.readFileSync(text).toString().trim().split('\n');
+
+const prefixTrie = new Trie();
+
+prefixTrie.populate(dictionary);
+
+console.log(prefixTrie.count());
+
 
 describe('TRIE', () => {
   let trie;
@@ -20,15 +31,27 @@ describe('TRIE', () => {
     expect(trie.root).to.equal(null);
   })
 
-  it.skip('should increase count for each new word instantiated', () => {
-    expect(trie.length).to.equal(0);
+  it('should increase count for each new word instantiated', () => {
+    expect(trie.wordCount).to.equal(0);
     trie.insert('whooohooo');
-    expect(trie.length).to.equal(1);
+    expect(trie.wordCount).to.equal(1);
   })
 
   it('should insert word', () => {
     trie.insert('hello');
     trie.insert('hellen');
-    console.log(JSON.stringify(trie, null, 4));
+    // console.log(JSON.stringify(trie, null, 4));
+  })
+
+  it('should return suggestions for search', () => {
+    trie.insert('hello');
+    trie.insert('hellocopter');
+    trie.insert('hellen');
+    expect(trie.suggest('he')).to.deep.equal(['hello', 'hellocopter', 'hellen']);
+  })
+
+  it('should populate with dictionary words', () => {
+    trie.populate(dictionary);
+    expect(trie.count()).to.equal(235886);
   })
 })
